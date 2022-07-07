5e729955928eedd5f186c3fa4a678fc175f7ac05
DukeHdez <briayan27@outlook.com>
2022-07-07 17:20:37 -0400

    Add comments

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const pizzaRoutes = require('./pizza.route');
+const commentRoutes = require('./comment-routes');
 
 // add prefix pf `/pizzas` to routes created in `pizza-routes.js`
 router.use('/pizzas', pizzaRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
